03fc0e7ba94da12dcbf9ea619e79f3a0350c5621
kiwijuice56 <alfaroeric127@gmail.com>
2024-02-17 00:50:09 -0500

    Finally removed all corner radius

--- game/main/ui/element_selector/element_selector.gd
+++ game/main/ui/element_selector/element_selector.gd
@@ -24,19 +24,6 @@ var simple: bool = false
 
 func _ready() -> void:
 	initialize_buttons()
-	for button in $basic/Basic.get_children():
-		if not button is ElementButton:
-			continue
-		# This is a remnant of a past decision to round the corners of some buttons
-		# It would be a lot of work to reset it on ALL of the button styles, so 
-		# for now it is OK to set it in the script
-		for style in ["normal", "pressed", "hover", "disabled"]:
-			if not button.get("theme_override_styles/" + style) == null:
-				button.get("theme_override_styles/" + style).corner_radius_top_left = 0
-				button.get("theme_override_styles/" + style).corner_radius_bottom_left = 0
-				button.get("theme_override_styles/" + style).corner_radius_top_right = 0
-				button.get("theme_override_styles/" + style).corner_radius_bottom_right = 0
-	
 	await get_tree().get_root().ready
 	update_custom_elements()
 
