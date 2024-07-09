f34e785ed4167a5287752a83ed817fb550be6f03
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-08 20:22:07 -0500

    Prettier buttons

--- game/main/ui/ui.gd
+++ game/main/ui/ui.gd
@@ -38,14 +38,15 @@ func _ready() -> void:
 	while len(c) > 0:
 		var x = c.pop_back()
 		if x is Button:
-			x.action_mode = BaseButton.ACTION_MODE_BUTTON_PRESS
+			x.action_mode = BaseButton.ACTION_MODE_BUTTON_RELEASE
 			if x is ElementButton:
 				x.pressed_long.connect(_on_button_down)
 			else:
 				x.pressed.connect(_on_button_down)
-			x.set("theme_override_styles/pressed", x.get("theme_override_styles/normal"))
-			x.set("theme_override_styles/hover", x.get("theme_override_styles/normal"))
-			x.set("action_mode", BaseButton.ActionMode.ACTION_MODE_BUTTON_RELEASE)
+			if not x.is_in_group("keep_style"):
+				x.set("theme_override_styles/pressed", x.get("theme_override_styles/normal"))
+				x.set("theme_override_styles/hover", x.get("theme_override_styles/normal"))
+				x.set("action_mode", BaseButton.ActionMode.ACTION_MODE_BUTTON_RELEASE)
 		if  x is TabBar:
 			x.tab_clicked.connect(_on_button_down)
 		c.append_array(x.get_children(true))
